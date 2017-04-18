52488330caf680a04172895d69b714b91cbfbd73
Kenny.H <nobi3892@gmail.com>
2017-04-18 13:31:41 +0900

    second

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -6,5 +6,12 @@ contract hello {
     function hello() {
         balance = 1000;
     }
+
+    function deposit(uint _value) returns(uint _newValue) {
+
+    	balance += _value;
+    	return balance;
+
+    }
     
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,9 +3,9 @@ var MetaCoin = artifacts.require("./MetaCoin.sol");
 var hello = artifacts.require("./hello.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  //deployer.deploy(ConvertLib);
+ // deployer.link(ConvertLib, MetaCoin);
+ // deployer.deploy(MetaCoin);
   deployer.deploy(hello);
 };
  
